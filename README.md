1f99517eb5f4c2e1feb233b696e6f0f6199ac497
Ayodeji Fakunle <ayodejifakunle@Ayodejis-MacBook-Air.local>
2022-12-01 17:47:54 +0100

    users can now play gamne by entering user input and also created a 4by4 grid

---
+++ README.md
@@ -0,0 +1,2 @@
+# Sliding Puzzle
+Built a sliding puzzle solution using Node Js with Express
--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const  express = require('express'); 
 const app  = express();
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
 
 
-const numbers = [[1,4,2], [3,0,5,], [6,7,8]];
-const orderedNumbers = [[0,1,2], [3,4,5], [6,7,8]];
 
-const direction = ["UP", "DOWN", "LEFT", "RIGHT"];
-for (let i = 0; i < numbers.length; i++) {
-  console.log(numbers[i]);
-  
-}
 
+//Created a list of input manually 
+const numbers = [[1,4,2,3], [5,6,7,8], [9,10,11,12], [13, 14, 15, 0]];
+const orderedNumbers = [[0,1,2,3], [4,5,6, 7], [8,9,10,11], [12,13,14, 15]];
+
+
+//A function to get the position of the number to be moved
 const getPosition =(numbers, number )=>{
   for (let x = 0; x < numbers.length; x++) {
         for (let y = 0; y < numbers[x].length; y++) {
@@ -24,24 +28,25 @@ const getPosition =(numbers, number )=>{
 }
 
 
-
+// a function to move a tile, this check for the kex pressed and move accordingly
 function moveTile(move) {
   if (JSON.stringify(numbers) === JSON.stringify(orderedNumbers)) {
-    console.log("You won!")
+    console.log("You won!!!!!!!")
+    rl.close();
   }else {
-    if (move === "UP") {
+    if (move === "UP" || move === "U" || move === "u" ) {
       up()
-    }else if (move === "DOWN") {
+    }else if (move === "DOWN" || move === "D" || move === "d") {
       down()
-    }else if (move === "RIGHT") {
+    }else if (move === "RIGHT" || move === "R" || move === "r") {
       right()
-    }else if (move === "LEFT") {
+    }else if (move === "LEFT" || move === "L" || move === "l") {
       left()
     }
   }
 }
 
-
+//Moves the number downwards
 function down() {
   const position = getPosition(numbers, 0);
   if (position.x - 1 < 0 ) {
@@ -52,10 +57,10 @@ function down() {
 
 }
 
-
+//Moves the number upwards
 function up (){
   const position = getPosition(numbers, 0);
-  if (position.x + 1 > 2 ) {
+  if (position.x + 1 > 3 ) {
       console.log("cant move")
   }else{
     swapPosition(position, {x:position.x + 1, y:position.y})
@@ -63,7 +68,7 @@ function up (){
 
 }
 
-
+//Moves the number to the right position
 function right(){
   const position = getPosition(numbers, 0);
   if (position.y - 1 < 0 ) {
@@ -74,10 +79,10 @@ function right(){
 
 }
 
-
+//Moves the number leftwards
 function left() {
   const position = getPosition(numbers, 0);
-  if (position.x + 1 > 2 ) {
+  if (position.x + 1 > 3 ) {
       console.log("cant move")
   }else{
     swapPosition(position, {x:position.x, y:position.y +1})
@@ -85,7 +90,7 @@ function left() {
 
 }
 
-
+//Swap the position of 0 and the numbers it wants to swap with.
 const swapPosition = (oldPosition, newPosition)=>{
   numbers[oldPosition.x][oldPosition.y] = numbers[newPosition.x][newPosition.y];
   numbers[newPosition.x][newPosition.y] = 0
@@ -96,19 +101,43 @@ const swapPosition = (oldPosition, newPosition)=>{
 }
 
 
-const moves = ["DOWN", "RIGHT", "END"]
 
-function startGame(moves) {
-  for (let index = 0; index < moves.length; index++) {
 
-    console.log(moves[index]);
-    moveTile(moves[index]);
-  }  
-
+function startGame() {
+  rl.question("Your Move ? ", function(move) {
+    moveTile(move);
+    if (JSON.stringify(numbers) === JSON.stringify(orderedNumbers)) {
+      console.log("You won!!!!!!!")
+      return rl.close();
+    }else{
+      startGame()
+    }
+  });
+    
 }
+
+
+rl.on("close", function() {
+  console.log("\nBYE BYE !!!");
+  process.exit(0);
+});
+
+
 app.listen(3000, () => {
     
-    console.log("Listening to app at 3000")
+    console.log("Starting the game, You can only move either R(Right), L(Left), U(Up), D(Down)");
+    console.log("PUZZLE!!!!!!!!!!!!!!!!!!!!!!!");
 
-    startGame(moves);
+    rl.question("Start Game ? ", function(answer) {
+      if (answer === "YES" || answer === "Yes" || answer === "y" || answer === "Y") {   
+        for (let i = 0; i < numbers.length; i++) {
+          console.log(numbers[i]);
+        }
+
+        startGame()
+      } else {
+        console.log("Game Over!!")
+      return rl.close();
+      }
+    });
   });
