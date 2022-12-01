c14f22b0f61c788f462d8e07c52e06d39f1f5f55
Ayodeji Fakunle <ayodejifakunle@Ayodejis-MacBook-Air.local>
2022-12-01 16:27:35 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,111 @@
+const  express = require('express'); 
+const app  = express();
+
+
+
+const numbers = [[1,4,2], [3,0,5,], [6,7,8]];
+const orderedNumbers = [[0,1,2], [3,4,5], [6,7,8]];
+
+const direction = ["UP", "DOWN", "LEFT", "RIGHT"];
+
+
+const getPosition =(numbers, number )=>{
+  for (let x = 0; x < numbers.length; x++) {
+        for (let y = 0; y < numbers[x].length; y++) {
+          if (numbers[x][y] === 0) {
+            return {x:x, y:y}
+          }
+        }
+    
+  }
+}
+
+
+
+function moveTile(move) {
+  if (JSON.stringify(numbers) === JSON.stringify(orderedNumbers)) {
+    console.log("You won!")
+  }else {
+    if (move === "UP") {
+      up()
+    }else if (move === "DOWN") {
+      down()
+    }else if (move === "RIGHT") {
+      right()
+    }else if (move === "LEFT") {
+      left()
+    }
+  }
+}
+
+
+function up() {
+  const position = getPosition(numbers, 0);
+  if (position.x - 1 < 0 ) {
+      console.log("cant move")
+  }else{
+    swapPosition(position, {x:position.x -1, y:position.y})
+  }
+
+}
+
+
+function down (){
+  const position = getPosition(numbers, 0);
+  if (position.x + 1 > 2 ) {
+      console.log("cant move")
+  }else{
+    swapPosition(position, {x:position.x + 1, y:position.y})
+  }
+
+}
+
+
+function left(){
+  const position = getPosition(numbers, 0);
+  if (position.y - 1 < 0 ) {
+      console.log("cant move")
+  }else{
+    swapPosition(position, {x:position.x, y:position.y -1})
+  }
+
+}
+
+
+function right() {
+  const position = getPosition(numbers, 0);
+  if (position.x + 1 > 2 ) {
+      console.log("cant move")
+  }else{
+    swapPosition(position, {x:position.x, y:position.y +1})
+  }
+
+}
+
+
+const swapPosition = (oldPosition, newPosition)=>{
+  numbers[oldPosition.x][oldPosition.y] = numbers[newPosition.x][newPosition.y];
+  numbers[newPosition.x][newPosition.y] = 0
+  for (let index = 0; index < numbers.length; index++) {
+    console.log(numbers[index])
+    
+  }
+}
+
+
+const moves = ["UP", "LEFT", "END"]
+
+function startGame(moves) {
+  for (let index = 0; index < moves.length; index++) {
+
+    console.log(moves[index]);
+    moveTile(moves[index]);
+  }  
+
+}
+app.listen(3000, () => {
+    
+    console.log("Listening to app at 3000")
+
+    startGame(moves);
+  });
